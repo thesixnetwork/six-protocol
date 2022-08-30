3973cf35082382c064e2b8cdae449a2e204eb5f5
dDee-D-six <dee.hamdee.084@gmail.com>
2022-08-30 18:14:15 +0700

    clean code

--- x/consume/keeper/msg_server_use_nft_by_evm.go
+++ x/consume/keeper/msg_server_use_nft_by_evm.go
@@ -2,21 +2,22 @@ package keeper
 
 import (
 	"context"
-	"fmt"
-	"strconv"
-	"time"
+	// "fmt"
+	// "strconv"
+	// "time"
 	"encoding/json"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/thesixnetwork/six-protocol/x/consume/types"
+	handler "github.com/thesixnetwork/six-protocol/handler"
 
 	// "crypto/ecdsa"
-	"bytes"
+	// "bytes"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto"
+	// "github.com/ethereum/go-ethereum/common"
+	// "github.com/ethereum/go-ethereum/common/hexutil"
+	// "github.com/ethereum/go-ethereum/crypto"
 )
 
 
@@ -45,60 +46,13 @@ func (k msgServer) UseNftByEVM(goCtx context.Context, msg *types.MsgUseNftByEVM)
 	var raw_sign_msg Message
     json.Unmarshal([]byte(msg.SignMessage), &raw_sign_msg)
 
-	// hash the message
-	sign_msg := "\x19Ethereum Signed Message:\n" + strconv.FormatInt(int64(len(msg.SignMessage)), 10) + msg.SignMessage
-
-	 // getting timestamp from message and transforming it to UTC
-	time_stamp := raw_sign_msg.Timestamp
-	_ = raw_sign_msg.Msg
-	msg_timestamp_int, err := strconv.ParseInt(time_stamp[:len(time_stamp)-3], 10, 64)
+	msg_timestamp_string, err := handler.ValidateTimestamp(raw_sign_msg.Timestamp)
 	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid message of timestamp")
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, err.Error())
 	}
-	msg_timestamp_epoch := time.Unix(msg_timestamp_int,0)
-
-	msg_timestamp_string := msg_timestamp_epoch.Format("2006-01-02T15:04:05Z")
-    _ ,error:= time.Parse(time.RFC3339,msg_timestamp_string)
-	if error != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid timestamp")
-	}
-
-	data := []byte(sign_msg)
-	hash := crypto.Keccak256Hash(data)
-	var hash_bytes = hash.Bytes()
-
-	//validate signature format
-	decode_signature, err := hexutil.Decode(msg.EthSignature)
+	_ ,err = handler.ValidateEVM(msg.SignMessage, msg.EthSignature, msg.EthAddress)
 	if err != nil {
-		// log.Fatalf("Failed to decode signature: %v", msg.Signature)
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature")
-	}
-	signature_with_revocery_id := decode_signature
-	// remove last byte coz is is recovery id
-	decode_signature[64] -= 27 // this on should be checked whether it can be a weak point later // remove recovery id
-
-	// get pulic key from signature
-	sigPublicKey, err := crypto.Ecrecover(hash_bytes, decode_signature) //recover publickey from signature and hash
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature or message")
-	}
-
-	// get address from public key
-	pubEDCA, err := crypto.UnmarshalPubkey(sigPublicKey)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "faild to unmarshal public key")
-	}
-	eth_address_from_pubkey := crypto.PubkeyToAddress(*pubEDCA)
-
-	eth_address := common.HexToAddress(msg.EthAddress)
-	if matches := bytes.Equal([]byte(eth_address_from_pubkey.Hex()), []byte(eth_address.Hex())); !matches {
-		var ret = fmt.Sprintf("eth_address_from_pubkey: %s ,eth_address %s", eth_address_from_pubkey.Hex(), eth_address.Hex())
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, ret)
-	}
-
-	signatureNoRecoverID := signature_with_revocery_id[:len(signature_with_revocery_id)-1] // remove recovery id
-	if verified := crypto.VerifySignature(sigPublicKey, hash.Bytes(), signatureNoRecoverID); !verified {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "verify failed")
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, err.Error())
 	}
 
 	var spend = types.MsgUseNft{
--- x/evmbind/keeper/msg_server_binding.go
+++ x/evmbind/keeper/msg_server_binding.go
@@ -2,18 +2,11 @@ package keeper
 
 import (
 	"context"
-	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/thesixnetwork/six-protocol/x/evmbind/types"
-
-	// "crypto/ecdsa"
-	"bytes"
-
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto"
+	handler "github.com/thesixnetwork/six-protocol/handler"
 )
 
 func (k msgServer) CreateBinding(goCtx context.Context, msg *types.MsgCreateBinding) (*types.MsgCreateBindingResponse, error) {
@@ -34,44 +27,9 @@ func (k msgServer) CreateBinding(goCtx context.Context, msg *types.MsgCreateBind
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, err.Error())
 	}
 
-	// hash the message
-	sign_msg := "\x19Ethereum Signed Message:\n" + strconv.FormatInt(int64(len(msg.SignMessage)), 10) + msg.SignMessage
-	data := []byte(sign_msg)
-	hash := crypto.Keccak256Hash(data)
-	var hash_bytes = hash.Bytes()
-
-	//validate signature format
-	decode_signature, err := hexutil.Decode(msg.EthSignature)
+	_ ,err = handler.ValidateEVM(msg.SignMessage, msg.EthSignature, msg.EthAddress)
 	if err != nil {
-		// log.Fatalf("Failed to decode signature: %v", msg.Signature)
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature")
-	}
-	signature_with_revocery_id := decode_signature
-	// remove last byte coz is is recovery id
-	decode_signature[64] -= 27 // this on should be checked whether it can be a weak point later // remove recovery id
-
-	// get pulic key from signature
-	sigPublicKey, err := crypto.Ecrecover(hash_bytes, decode_signature) //recover publickey from signature and hash
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature or message")
-	}
-	// pubKey_encode := hexutil.Encode(sigPublicKey)
-
-	// get address from public key
-	pubEDCA, err := crypto.UnmarshalPubkey(sigPublicKey)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "faild to unmarshal public key")
-	}
-	eth_address_from_pubkey := crypto.PubkeyToAddress(*pubEDCA)
-
-	eth_address := common.HexToAddress(msg.EthAddress)
-	if matches := bytes.Equal([]byte(eth_address_from_pubkey.Hex()), []byte(eth_address.Hex())); !matches {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "fail to validate eth address, check signature and message are correct")
-	}
-
-	signatureNoRecoverID := signature_with_revocery_id[:len(signature_with_revocery_id)-1] // remove recovery id
-	if verified := crypto.VerifySignature(sigPublicKey, hash.Bytes(), signatureNoRecoverID); !verified {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "verify failed")
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, err.Error())
 	}
 
 	var binding = types.Binding{
@@ -100,39 +58,14 @@ func (k msgServer) UpdateBinding(goCtx context.Context, msg *types.MsgUpdateBind
 		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, "index not set")
 	}
 
+	_ ,err := handler.ValidateEVM(msg.SignMessage, msg.EthSignature, msg.EthAddress)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, err.Error())
+	}
+
 	// Checks if the the msg creator is the same as the current owner
 	if msg.Creator != valFound.Creator {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
-	}
-	// hash the message
-	data := []byte(msg.SignMessage)
-	hash := crypto.Keccak256Hash(data)
-	var hash_bytes = hash.Bytes()
-
-	//validate signature format
-	decode_signature, err := hexutil.Decode(msg.EthSignature)
-	if err != nil {
-		// log.Fatalf("Failed to decode signature: %v", msg.Signature)
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature")
-	}
-
-	// get pulic key from signature
-	sigPublicKey, err := crypto.Ecrecover(hash_bytes, decode_signature) //recover publickey from signature and hash
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature or message")
-	}
-	// pubKey_encode := hexutil.Encode(sigPublicKey)
-
-	// get address from public key
-	pubEDCA, err := crypto.UnmarshalPubkey(sigPublicKey)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "faild to unmarshal public key")
-	}
-	eth_address_from_pubkey := crypto.PubkeyToAddress(*pubEDCA)
-
-	eth_address := common.HexToAddress(msg.EthAddress)
-	if matches := bytes.Equal([]byte(eth_address_from_pubkey.Hex()), []byte(eth_address.Hex())); !matches {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "fail to validate eth address, check signature and message are correct")
 	}
 
 	var binding = types.Binding{
