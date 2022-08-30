d71414874c2118a709cf6440bd0f35810889782e
dDee-D-six <dee.hamdee.084@gmail.com>
2022-08-30 18:26:21 +0700

    clean code

--- handler/evm_validate.go
+++ handler/evm_validate.go
@@ -14,7 +14,7 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-func ValidateEVM(message string, signature string, eth_address string) (pass bool,err error) {
+func ValidateEVM(message string, signature string, eth_address string) (_signature []byte,err error) {
     sign_msg := "\x19Ethereum Signed Message:\n" + strconv.FormatInt(int64(len(message)), 10) + message
 
 	data := []byte(sign_msg)
@@ -25,7 +25,7 @@ func ValidateEVM(message string, signature string, eth_address string) (pass boo
 	decode_signature, err := hexutil.Decode(signature)
 	if err != nil {
 		// log.Fatalf("Failed to decode signature: %v", msg.Signature)
-		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature")
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature")
 	}
 	signature_with_revocery_id := decode_signature
 	// remove last byte coz is is recovery id
@@ -34,28 +34,28 @@ func ValidateEVM(message string, signature string, eth_address string) (pass boo
 	// get pulic key from signature
 	sigPublicKey, err := crypto.Ecrecover(hash_bytes, decode_signature) //recover publickey from signature and hash
 	if err != nil {
-		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature or message")
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "invalid signature or message")
 	}
 
 	// get address from public key
 	pubEDCA, err := crypto.UnmarshalPubkey(sigPublicKey)
 	if err != nil {
-		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "faild to unmarshal public key")
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "faild to unmarshal public key")
 	}
 	eth_address_from_pubkey := crypto.PubkeyToAddress(*pubEDCA)
 
 	common_eth_address := common.HexToAddress(eth_address)
 	if matches := bytes.Equal([]byte(eth_address_from_pubkey.Hex()), []byte(common_eth_address.Hex())); !matches {
 		var ret = fmt.Sprintf("eth_address_from_pubkey: %s ,eth_address %s", eth_address_from_pubkey.Hex(), common_eth_address.Hex())
-		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, ret)
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, ret)
 	}
 
 	signatureNoRecoverID := signature_with_revocery_id[:len(signature_with_revocery_id)-1] // remove recovery id
 	if verified := crypto.VerifySignature(sigPublicKey, hash.Bytes(), signatureNoRecoverID); !verified {
-		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "verify failed")
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "verify failed")
 	}
 	
-	return true, nil
+	return signatureNoRecoverID, nil
 }
 
 func ValidateTimestamp(timestamp string) (utc_time_to_string string, err error) {
