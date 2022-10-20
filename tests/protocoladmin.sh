9874c06f8f1df45121f184344bd755f16f2ca7fe
dDee-D-six <dee.hamdee.084@gmail.com>
2022-10-20 18:16:43 +0700

    update script relate to config.yml

--- tests/protocoladmin.sh
+++ tests/protocoladmin.sh
@@ -1,33 +1,34 @@
 # create-group
-if sixd tx protocoladmin create-group artist --chain-id testnet --from alice -y | grep -q 'msg_index: 0'; then
+if sixd tx protocoladmin create-group artist --chain-id testnet --from super-admin -y | grep -q 'msg_index: 0'; then
     echo "create-group success"
 else
     echo "create-group failed"
 fi
 
 # add-admin-to-group
-if sixd tx protocoladmin add-admin-to-group artist 6x1myrlxmmasv6yq4axrxmdswj9kv5gc0ppx95rmq --chain-id testnet --from alice -y | grep -q 'msg_index: 0'; then
+if sixd tx protocoladmin add-admin-to-group artist $(sixd keys show -a alice) --chain-id testnet --from super-admin -y | grep -q 'msg_index: 0'; then
     echo "add-admin-to-group success"
 else
     echo "add-admin-to-group failed"
 fi
 
 # add-admin-to-group
-if sixd tx protocoladmin add-admin-to-group artist 6x13g50hqdqsjk85fmgqz2h5xdxq49lsmjdwlemsp --chain-id testnet --from alice -y | grep -q 'msg_index: 0'; then
+if sixd tx protocoladmin add-admin-to-group artist $(sixd keys show -a bob) --chain-id testnet --from super-admin -y | grep -q 'msg_index: 0'; then
     echo "add-admin-to-group success"
 else
     echo "add-admin-to-group failed"
 fi
 
 # remove-admin-from-group
-if sixd tx protocoladmin remove-admin-from-group artist 6x13g50hqdqsjk85fmgqz2h5xdxq49lsmjdwlemsp --chain-id testnet --from alice -y | grep -q 'msg_index: 0'; then
+if sixd tx protocoladmin remove-admin-from-group artist $(sixd keys show -a bob) --chain-id testnet --from super-admin -y | grep -q 'msg_index: 0'; then
     echo "remove-admin-from-group success"
 else
     echo "remove-admin-from-group failed"
 fi
 
 # delete-group
-if sixd tx protocoladmin delete-group artist --chain-id testnet --from alice -y | grep -q 'msg_index: 0'; then
+sixd tx protocoladmin create-group artist_test --chain-id testnet --from super-admin -y
+if sixd tx protocoladmin delete-group artist_test --chain-id testnet --from super-admin -y | grep -q 'msg_index: 0'; then
     echo "delete-group success"
 else
     echo "delete-group failed"
