b5af49f8c49bad510788bd18d9a36cf5ce238f52
dDee-D-six <dee.hamdee.084@gmail.com>
2022-11-07 10:46:08 +0700

    init chain

--- docker/init.sh
+++ docker/init.sh
@@ -1,6 +1,6 @@
 export CHAIN_ID=six
 export MONIKER=sixnode1 ## should be: export as docker env var
-export VALKEY=validator
+export VALKEY=val0 # should be: export as docker env var
 export SIX_HOME=./build/six_home
 VAL0_MNEMONIC="note base stone list envelope tail start forget alarm acoustic cook occur divert giant bike curtain chase shuffle fade glow capital slot file provide"
 VAL1_MNEMONIC="strike tower consider despair bridge diesel clay celery violin base hello ride they weather tunnel elite truth oblige spot hen wise flag pet battle"
@@ -12,7 +12,7 @@ rm -Rf ${SIX_HOME}
 
 sixd init ${MONIKER} --chain-id=${CHAIN_ID} --home ${SIX_HOME}
 
-# sixd keys add ${VALKEY} --keyring-backend test --home ${SIX_HOME}
+# mint to validator
 echo $SUPER_ADMIN_MNEMONIC | sixd keys add super-admin --recover --home ${SIX_HOME} --keyring-backend test
 echo $VAL0_MNEMONIC | sixd keys add val0 --recover --home ${SIX_HOME} --keyring-backend test
 echo $VAL1_MNEMONIC | sixd keys add val1 --recover --home ${SIX_HOME} --keyring-backend test
@@ -29,10 +29,5 @@ sixd add-genesis-account $(sixd keys show -a super-admin --keyring-backend=test 
 jq '.app_state.protocoladmin.groupList[0] |= . + {"name": "super.admin", "owner": "'`$SUPERADMIN_ADDRESS`'"}' ${SIX_HOME}/config/genesis.json | sponge ${SIX_HOME}/config/genesis.json
 jq '.app_state.protocoladmin.adminList[0] |= . + {"admin": "'"$SUPERADMIN_ADDRESS"'", "group": "super.admin"}' ${SIX_HOME}/config/genesis.json | sponge ${SIX_HOME}/config/genesis.json
 
-sixd gentx val0 100000000stake --chain-id=six --keyring-backend=test --home ${SIX_HOME}
+sixd gentx ${VALKEY} 100000000stake --chain-id=six --keyring-backend=test --home ${SIX_HOME}
 sixd collect-gentxs --home ${SIX_HOME}
-
-# # start
-sixd start --home ${SIX_HOME}
-
-# address: 0x837f7E4DcCB7AEd3807e51f0535E7Fa2718bc0E7
