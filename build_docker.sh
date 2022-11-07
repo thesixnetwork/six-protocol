96f30ec67013444aa588aea07a05ce5ecce04308
dDee-D-six <dee.hamdee.084@gmail.com>
2022-11-07 11:41:45 +0700

    update doker script

---
+++ build_docker.sh
@@ -0,0 +1,2 @@
+GITHUB_TOKEN=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
+docker build . -t six/node --build-arg GITHUB_TOKEN=${GITHUB_TOKEN}
--- docker_build.sh
+++
@@ -1,2 +0,0 @@
-DGITHUB_TOKEN=<Put your token here>
-dk build . -t six/node --build-arg GITHUB_TOKEN=$GITHUB_TOKEN
