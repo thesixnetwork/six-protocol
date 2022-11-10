4de6a54a5da0e4f390727725f212ecccecb78269
dDee-D-six <dee.hamdee.084@gmail.com>
2022-11-10 18:51:12 +0700

    able to connect accros validator

--- docker/init.sh
+++ docker/init.sh
@@ -1,11 +1,17 @@
+MONIKER=$1
 export CHAIN_ID=six
-export MONIKER=sixnode1 ## should be: export as docker env var
-export VALKEY=val0 # should be: export as docker env var
+export VALKEY=val1 # should be: export as docker env var
 export SIX_HOME=./build/six_home
-VAL0_MNEMONIC="note base stone list envelope tail start forget alarm acoustic cook occur divert giant bike curtain chase shuffle fade glow capital slot file provide"
-VAL1_MNEMONIC="strike tower consider despair bridge diesel clay celery violin base hello ride they weather tunnel elite truth oblige spot hen wise flag pet battle"
-VAL2_MNEMONIC="canvas human require month loan oak december blame grit palm slice error absorb total spice autumn trouble soda repeat shove quit bid forward organ"
-VAL3_MNEMONIC="grant raw marine drink text dove flat waste wish buzz output hand merge cluster civil clog stay alert silent reunion idea cake village almost"
+ALICE_MNEMONIC="history perfect across group seek acoustic delay captain sauce audit carpet tattoo exhaust green there giant cluster want pond bulk close screen scissors remind"
+BOB_MNEMONIC="limb sister humor wisdom elephant weasel beyond must any desert glance stem reform soccer include chest chef clerk call popular display nerve priority venture"
+VAL1_MNEMONIC="note base stone list envelope tail start forget alarm acoustic cook occur divert giant bike curtain chase shuffle fade glow capital slot file provide"
+VAL2_MNEMONIC="strike tower consider despair bridge diesel clay celery violin base hello ride they weather tunnel elite truth oblige spot hen wise flag pet battle"
+VAL3_MNEMONIC="canvas human require month loan oak december blame grit palm slice error absorb total spice autumn trouble soda repeat shove quit bid forward organ"
+VAL4_MNEMONIC="grant raw marine drink text dove flat waste wish buzz output hand merge cluster civil clog stay alert silent reunion idea cake village almost"
+ORACLE1_MNEMONIC="list split future remain scene cheap pledge forum siren purse bright ivory split morning swing dumb fabric rapid remove worth diary task island donkey"
+ORACLE2_MNEMONIC="achieve rice anger junk delay glove slam find poem feed emerge next core twice kitchen road proof remain notice slice walk super piece father"
+ORACLE3_MNEMONIC="hint expose mix lemon leave genuine host fiction peasant daughter enable region mixture bean soda auction armed turtle iron become bracket wasp drama front"
+ORACLE4_MNEMONIC="clown cabbage clean design mosquito surround citizen virus kite castle sponsor wife lesson coffee alien panel hand together good crazy fabric mouse hat town"
 SUPER_ADMIN_MNEMONIC="expect peace defense conduct virtual flight flip unit equip solve broccoli protect shed group else useless tree such tornado minimum decade tower warfare galaxy"
 
 rm -Rf ${SIX_HOME}
@@ -14,20 +20,32 @@ sixd init ${MONIKER} --chain-id=${CHAIN_ID} --home ${SIX_HOME}
 
 # mint to validator
 echo $SUPER_ADMIN_MNEMONIC | sixd keys add super-admin --recover --home ${SIX_HOME} --keyring-backend test
-echo $VAL0_MNEMONIC | sixd keys add val0 --recover --home ${SIX_HOME} --keyring-backend test
+echo $ALICE_MNEMONIC | sixd keys add alice --recover --home ${SIX_HOME} --keyring-backend test
+echo $BOB_MNEMONIC | sixd keys add bob --recover --home ${SIX_HOME} --keyring-backend test
 echo $VAL1_MNEMONIC | sixd keys add val1 --recover --home ${SIX_HOME} --keyring-backend test
 echo $VAL2_MNEMONIC | sixd keys add val2 --recover --home ${SIX_HOME} --keyring-backend test
 echo $VAL3_MNEMONIC | sixd keys add val3 --recover --home ${SIX_HOME} --keyring-backend test
+echo $VAL4_MNEMONIC | sixd keys add val4 --recover --home ${SIX_HOME} --keyring-backend test
+echo $ORACLE1_MNEMONIC | sixd keys add oracle1 --recover --home ${SIX_HOME} --keyring-backend test
+echo $ORACLE2_MNEMONIC | sixd keys add oracle2 --recover --home ${SIX_HOME} --keyring-backend test
+echo $ORACLE3_MNEMONIC | sixd keys add oracle3 --recover --home ${SIX_HOME} --keyring-backend test
+echo $ORACLE4_MNEMONIC | sixd keys add oracle4 --recover --home ${SIX_HOME} --keyring-backend test
 
-sixd add-genesis-account $(sixd keys show -a val0 --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
 sixd add-genesis-account $(sixd keys show -a val1 --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
 sixd add-genesis-account $(sixd keys show -a val2 --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
 sixd add-genesis-account $(sixd keys show -a val3 --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
+sixd add-genesis-account $(sixd keys show -a val4 --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
+sixd add-genesis-account $(sixd keys show -a oracle1 --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
+sixd add-genesis-account $(sixd keys show -a oracle2 --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
+sixd add-genesis-account $(sixd keys show -a oracle3 --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
+sixd add-genesis-account $(sixd keys show -a oracle4 --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
+sixd add-genesis-account $(sixd keys show -a alice --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
+sixd add-genesis-account $(sixd keys show -a bob --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
 sixd add-genesis-account $(sixd keys show -a super-admin --keyring-backend=test --home ${SIX_HOME}) 1000000000000usix --keyring-backend test --home ${SIX_HOME}
 
-# add super.admin to grouplist
-jq '.app_state.protocoladmin.groupList[0] |= . + {"name": "super.admin", "owner": "'`$SUPERADMIN_ADDRESS`'"}' ${SIX_HOME}/config/genesis.json | sponge ${SIX_HOME}/config/genesis.json
-jq '.app_state.protocoladmin.adminList[0] |= . + {"admin": "'"$SUPERADMIN_ADDRESS"'", "group": "super.admin"}' ${SIX_HOME}/config/genesis.json | sponge ${SIX_HOME}/config/genesis.json
+# # add super.admin to grouplist
+# jq '.app_state.protocoladmin.groupList[0] |= . + {"name": "super.admin", "owner": "'`$SUPERADMIN_ADDRESS`'"}' ${SIX_HOME}/config/genesis.json | sponge ${SIX_HOME}/config/genesis.json
+# jq '.app_state.protocoladmin.adminList[0] |= . + {"admin": "'"$SUPERADMIN_ADDRESS"'", "group": "super.admin"}' ${SIX_HOME}/config/genesis.json | sponge ${SIX_HOME}/config/genesis.json
 
 sixd gentx ${VALKEY} 100000000usix --chain-id=six --keyring-backend=test --home ${SIX_HOME}
 sixd collect-gentxs --home ${SIX_HOME}
--- setup_genesis.sh
+++ setup_genesis.sh
@@ -1,34 +1,19 @@
-default_github_token=$1
 default_six_home=six_home
-default_docker_tag="latest"
-node_homes=(
-    sixnode0
-    sixnode1
-    sixnode2
-    sixnode3
-);
-validator_keys=(
-    val1
-    val2
-    val3
-    val4
-);
+SIX_HOME=$1
+if [ -z "$SIX_HOME" ]; then
+    SIX_HOME=$default_six_home
+fi
 
+## funtion setup_genesis
 function setupGenesis() {
-    echo "#######################################"
-    echo "Setup ${SIX_HOME} genesis..."
-    echo "#######################################"
     NODE_PEER=$(jq '.app_state.genutil.gen_txs[0].body.memo' ./build/sixnode0/config/genesis.json)
 
-    if [[ ${SIX_HOME} == "sixnode0" ]]; then
+    if [[ SIX_HOME=="sixnode0"]]; then
         echo "sixnode0"
-        NODE_PEER=$(jq '.app_state.genutil.gen_txs[0].body.memo' ./build/sixnode1/config/genesis.json)
-        sed -i '' "s/persistent_peers = \"\"/persistent_peers = ${NODE_PEER}/g" ./build/${SIX_HOME}/config/config.toml
     else
         ## replace NODE_PEER in config.toml to persistent_peers
         sed -i '' "s/persistent_peers = \"\"/persistent_peers = ${NODE_PEER}/g" ./build/${SIX_HOME}/config/config.toml
     fi
-
     ## replace to enalbe api
     sed -i '' "108s/.*/enable = true/" ./build/${SIX_HOME}/config/app.toml
 
@@ -72,98 +57,8 @@ function setupGenesis() {
 
 }
 
-echo "#############################################"
-echo "## 1. Build Docker Image                   ##"
-echo "## 2. Docker Compose init chain            ##"
-echo "## 3. Start chain validator                ##"
-echo "## 4. Stop chain validator                 ##"
-echo "## 5. Config Genesis                       ##"
-echo "## 6. Reset chain validator                ##"
-echo "## 7. Staking validator                    ##"
-echo "#############################################"
-
-read -p "Enter your choice: " choice
-case $choice in
-    1)
-        echo "Building Docker Image"
-        read -p "Enter Github Token: " github_token 
-        read -p "Enter Docker Tag: " docker_tag
-        if [ -z "$github_token" ]; then
-            github_token=$default_github_token
-        fi
-        if [ -z "$docker_tag" ]; then
-            docker_tag=$default_docker_tag
-        fi
-        docker build . -t six/node:${docker_tag} --build-arg GITHUB_TOKEN=${github_token}
-        ;;
-    2)
-        echo "Run init Chain validator"
-        export COMMAND="init"
-        docker compose -f ./docker-compose.yml up
-        ;;
-    3)
-        echo "Running Docker Container in Interactive Mode"
-        export COMMAND="start_chain"
-        docker compose -f ./docker-compose.yml up -d
-        ;;
-    4)
-        echo "Stop Docker Container"
-        export COMMAND="start_chain"
-        docker compose -f ./docker-compose.yml down
-        ;;
-    5) 
-        echo "Config Genesis"
-        for home in ${node_homes[@]}
-        do  
-            (
-            export SIX_HOME=${home}
-            if [[ -e !./build/sixnode0/config/genesis.json ]]; then
-                echo "File does not exist 🖕"
-            else
-                setupGenesis
-            fi 
-            )|| exit 1
-        done
-        ;;
-    6) 
-        echo "Reset Docker Container"
-        for home in ${node_homes[@]}
-        do
-            echo "#######################################"
-            echo "Starting ${home} reset..."
-            echo "#######################################"
-            ( export DAEMON_HOME=./build/${home}
-            rm -rf $DAEMON_HOME/data
-            rm -rf $DAEMON_HOME/wasm
-            rm $DAEMON_HOME/config/addrbook.json
-            mkdir $DAEMON_HOME/data/
-            touch $DAEMON_HOME/data/priv_validator_state.json
-            echo '{"height": "0", "round": 0,"step": 0}' > $DAEMON_HOME/data/priv_validator_state.json
-            )|| exit 1
-        done
-        ;;
-    7)
-        echo "Staking Docker Container"
-        i=0
-        amount=100000001
-        for val in ${validator_keys[@]:1:3}
-        # for val in ${validator_keys[@]}
-        do
-            echo "#######################################"
-            ( 
-            echo "Creating validators ${val}"
-            echo ${node_homes[i]}
-            export DAEMON_HOME=./build/${node_homes[i]}
-            sixd tx staking create-validator --amount="${amount}usix" --from=${val} --moniker ${node_homes[i]} \
-                --pubkey $(sixd tendermint show-validator --home ./build/${node_homes[i]}) --home build/${node_homes[i]} \
-                --keyring-backend test --commission-rate 0.1 --commission-max-rate 0.5 --commission-max-change-rate 0.1 \
-                --min-self-delegation 1000000 --node http://0.0.0.0:26662 -y --min-delegation 1000000 --delegation-increment 1000000
-            ) || exit 1
-            i=$((i+1))
-            amount=$((amount+1))
-        done
-        ;;
-    *)
-        echo "Invalid Choice"
-        ;;
-esac
+if [[ -e !./build/sixnode0/config/genesis.json ]]; then
+    echo "File does not exist 🖕"
+else
+    setupGenesis
+fi
