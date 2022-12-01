2b2695e8befa299d1956db0a513d0e02121ed565
dDee-D-six <dee.hamdee.084@gmail.com>
2022-12-01 13:54:03 +0700

    add ethermint and feemaker

---
+++ app/prefix.go
@@ -0,0 +1,16 @@
+package app
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	cmdcfg "github.com/thesixnetwork/six-protocol/cmd/sixd/config"
+	ethcfg "github.com/evmos/ethermint/cmd/config"
+)
+
+func SetConfig() {
+	config := sdk.GetConfig()
+	cmdcfg.SetBech32Prefixes(config)
+	ethcfg.SetBip44CoinType(config)
+	// Make sure address is compatible with ethereum
+	config.SetAddressVerifier(VerifyAddressFormat)
+	config.Seal()
+}
