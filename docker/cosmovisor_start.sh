76a9d52dbff185785c71fd2ad19a696273942f06
dDee-D-six <dee.hamdee.084@gmail.com>
2022-12-02 17:43:43 +0700

    update script

--- docker/cosmovisor_start.sh
+++ docker/cosmovisor_start.sh
@@ -1,4 +1,4 @@
 export DAEMON_NAME=sixd         
-export DAEMON_HOME=/opt/build/six_home
+export DAEMON_HOME=$HOME/.six
 export DAEMON_RESTART_AFTER_UPGRADE=true
 cosmovisor run start --home $DAEMON_HOME 
