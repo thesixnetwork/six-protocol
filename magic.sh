1653539721f53e638801894b3dfd9a73cfbefaad
dDee-D-six <dee.hamdee.084@gmail.com>
2022-12-21 20:35:30 +0700

    :hammer: magic script

--- magic.sh
+++ magic.sh
@@ -4,5 +4,6 @@ if [ -z "$TOKEN"]; then
 fi
 rm go.sum
 touch go.sum
-git config --global url."https://${TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
+git config --global url."https://${TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/" ##! To make it able to download private repo
 go mod tidy -e -go=1.17 && go mod tidy -e -go=1.18
+git config --global --unset url."https://${TOKEN}:x-oauth-basic@github.com/".insteadOf  ##! To prevent it from cannot access to other repo
