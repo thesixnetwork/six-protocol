46c20f73e564d27f9fff3e991c8a40a20aa69f9e
dDee-D-six <dee.hamdee.084@gmail.com>
2022-12-27 11:11:42 +0700

    :hammer: gitbook script

--- scripts/gitbook/init_data.sh
+++ scripts/gitbook/init_data.sh
@@ -1,5 +1,5 @@
-RPC_ENDPOINT=http://localhost:26657
-CHAIN_ID=six
+RPC_ENDPOINT=https://rpc1.fivenet.sixprotocol.net:443
+CHAIN_ID=fivenet
 BASE64_SCHEMA=`cat nft-schema.json | base64 | tr -d '\n'`
 
 sixd tx nftmngr create-nft-schema --from alice --gas auto --gas-adjustment 1.5 --gas-prices 1.25usix -y --chain-id ${CHAIN_ID} \
