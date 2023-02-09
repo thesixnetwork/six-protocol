4c2d11e577d7ad552c7a4c249161d6ce1adca65c
dDee-D-six <dee.hamdee.084@gmail.com>
2023-02-09 22:26:58 +0700

    :wrench: cosmovisor update

--- docker/cosmovisor_start.sh
+++ docker/cosmovisor_start.sh
@@ -1,4 +1,4 @@
 export DAEMON_NAME=sixd         
 export DAEMON_HOME=/opt/build/six_home
 export DAEMON_RESTART_AFTER_UPGRADE=true
-cosmovisor run start --home $DAEMON_HOME  --minimum-gas-prices 1.25usix
+cosmovisor start --home $DAEMON_HOME  --minimum-gas-prices 1.25usix
