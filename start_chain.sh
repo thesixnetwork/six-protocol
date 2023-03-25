229f30c094c105c381c1538d21f57b5e1c322577
dDee-D-six <dee.hamdee.084@gmail.com>
2023-03-25 22:12:35 +0700

    :hammer: start chain no need to use private repo anymore

--- start_chain.sh
+++ start_chain.sh
@@ -1,5 +1,5 @@
 rm ./docs/static/openapi.yml
 rm go.sum && touch go.sum
-export GOPRIVATE=github.com/thesixnetwork/sixnft
+# export GOPRIVATE=github.com/thesixnetwork/sixnft
 go mod tidy -e -go=1.17 && go mod tidy -e -go=1.18
 ignite chain serve --config ./config.yml -r -f -v
