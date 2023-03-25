f3a43200113202470d17400b25669648012ad806
dDee-D-six <dee.hamdee.084@gmail.com>
2023-03-25 23:43:38 +0700

    :hammer: prepare test on docker

--- magic.sh
+++
@@ -1,7 +0,0 @@
-TOKEN=$1
-rm go.sum
-touch go.sum
-export GOPRIVATE=github.com/thesixnetwork/sixnft ##! To make it able to download private repo
-git config --global url."https://${TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/" ##! To make it able to download private repo
-go mod tidy -e -go=1.17 && go mod tidy -e -go=1.18
-git config --global --unset url."https://${TOKEN}:x-oauth-basic@github.com/".insteadOf  ##! To prevent it from cannot access to other repo
