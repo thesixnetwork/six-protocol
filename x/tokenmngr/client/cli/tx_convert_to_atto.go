d4e90f1e2765ad6789b0044066328f48d50e9938
dDee-D-six <dee.hamdee.084@gmail.com>
2023-03-30 17:58:21 +0700

    :sparkles: convert to atto to hex address or any 6x address is done

--- x/tokenmngr/client/cli/tx_convert_to_atto.go
+++ x/tokenmngr/client/cli/tx_convert_to_atto.go
@@ -1,12 +1,14 @@
 package cli
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	ethermint "github.com/evmos/ethermint/types"
 	"github.com/spf13/cobra"
 	"github.com/thesixnetwork/six-protocol/x/tokenmngr/types"
 )
@@ -15,9 +17,9 @@ var _ = strconv.Itoa(0)
 
 func CmdConvertToAtto() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "convert-to-atto [amount]",
+		Use:   "convert-to-atto [amount] [RECEIVER(optional)]",
 		Short: "Convert native token from 10^6 to native token 10^18 for usign with Evm",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.RangeArgs(1, 2),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			coins, err := sdk.ParseCoinNormalized(args[0])
 			if err != nil {
@@ -29,9 +31,24 @@ func CmdConvertToAtto() *cobra.Command {
 				return err
 			}
 
+			creator := clientCtx.GetFromAddress().String()
+			var receiver string
+			if len(args) == 2 {
+				receiver = args[1]
+				if err := ethermint.ValidateAddress(receiver); err != nil {
+					_, err := sdk.AccAddressFromBech32(receiver)
+					if err != nil {
+						return fmt.Errorf("invalid receiver address: %s", err)
+					}
+				}
+			} else {
+				receiver = creator
+			}
+
 			msg := types.NewMsgConvertToAtto(
-				clientCtx.GetFromAddress().String(),
+				creator,
 				coins,
+				receiver,
 			)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
--- x/tokenmngr/keeper/msg_server_convert_to_atto.go
+++ x/tokenmngr/keeper/msg_server_convert_to_atto.go
@@ -5,6 +5,8 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/ethereum/go-ethereum/common"
+	ethermint "github.com/evmos/ethermint/types"
 	"github.com/thesixnetwork/six-protocol/x/tokenmngr/types"
 )
 
@@ -19,23 +21,42 @@ func (k msgServer) ConvertToAtto(goCtx context.Context, msg *types.MsgConvertToA
 		return nil, err
 	}
 
+	// check that receiver is cosmos address or ethereum address
+	var addr []byte
+	receiver, err := sdk.AccAddressFromBech32(msg.Receiver)
+	if err != nil {
+		if err := ethermint.ValidateAddress(msg.Receiver); err != nil {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "receiver address is not cosmos or ethereum address")
+		}
+
+		if common.IsHexAddress(msg.Receiver){
+			addr = common.HexToAddress(msg.Receiver).Bytes()
+		}
+		receiver = sdk.AccAddress(addr)
+	}
+
+	// Check is this token is exist in token list
 	token, foundToken := k.GetToken(ctx, msg.Amount.Denom)
 	if !foundToken {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, "token does not exist")
 	}
 
+	// accept only usix token to convert to atto or asix
 	if token.Base != "usix" {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "token is not usix")
 	}
 
+	// Check is this amount is valid
 	if msg.Amount.Amount.IsZero() {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "amount of token is prohibit from module")
 	}
 
+	// Check token balance of creator
 	if balance := k.bankKeeper.GetBalance(ctx, signer, denom); balance.Amount.LT(msg.Amount.Amount) {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Amount of token is too high than current balance")
 	}
 
+	// Check token supply
 	supply := k.bankKeeper.GetSupply(ctx, denom)
 	if supply.Amount.LT(msg.Amount.Amount) {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "amount of token is higher than current total supply")
@@ -57,7 +78,7 @@ func (k msgServer) ConvertToAtto(goCtx context.Context, msg *types.MsgConvertToA
 
 	// send to receiver
 	if err := k.bankKeeper.SendCoinsFromModuleToAccount(
-		ctx, types.ModuleName, signer, sdk.NewCoins(attoAmount),
+		ctx, types.ModuleName, receiver, sdk.NewCoins(attoAmount),
 	); err != nil {
 		return nil, sdkerrors.Wrap(types.ErrSendCoinsFromAccountToModule, "unable to send msg.Amounts from module to account despite previously minting msg.Amounts to module account:"+err.Error())
 	}
