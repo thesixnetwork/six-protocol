ff5a96c923a5c4ae13cc080bf6a50db3f38413e9
dDee-D-six <dee.hamdee.084@gmail.com>
2023-04-04 01:10:56 +0700

    :sparkles: switch on/off converting contract

--- x/tokenmngr/client/cli/tx.go
+++ x/tokenmngr/client/cli/tx.go
@@ -43,6 +43,7 @@ func GetTxCmd() *cobra.Command {
 	cmd.AddCommand(CmdConvertToAtto())
 	cmd.AddCommand(CmdConvertToMicro())
 	cmd.AddCommand(CmdSetConverterParams())
+	cmd.AddCommand(CmdEnableContractConverter())
 	// this line is used by starport scaffolding # 1
 
 	return cmd
