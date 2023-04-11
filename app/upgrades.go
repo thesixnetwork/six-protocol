1b3c7b9261c008ffb80aebd8661896af51ab9f6b
dDee-D-six <dee.hamdee.084@gmail.com>
2023-04-11 14:17:51 +0700

    :test_tube: for fivenet

--- app/upgrades.go
+++ app/upgrades.go
@@ -55,7 +55,7 @@ func (app *App) VersionTriggerFivenet() {
 func (app *App) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(UpgradeName, func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 
-		if ctx.ChainID() == "five" {
+		if ctx.ChainID() == "fivenet" {
 			app.VersionTriggerFivenet()
 		} else {
 			app.VersionTrigger()
