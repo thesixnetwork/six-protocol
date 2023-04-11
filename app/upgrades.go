b513f7fba936bc5ad352d676673a0732b78c2739
dDee-D-six <dee.hamdee.084@gmail.com>
2023-04-11 12:50:23 +0700

    :test_tube: test mingration

--- app/upgrades.go
+++ app/upgrades.go
@@ -54,6 +54,12 @@ func (app *App) VersionTriggerFivenet() {
 
 func (app *App) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(UpgradeName, func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+
+		if ctx.ChainID() == "five" {
+			app.VersionTriggerFivenet()
+		} else {
+			app.VersionTrigger()
+		}
 		// * For MAINNET ONLY *
 		// * Becase TESTNET We already have this version of data *
 		// * Module Tokenmngr *
