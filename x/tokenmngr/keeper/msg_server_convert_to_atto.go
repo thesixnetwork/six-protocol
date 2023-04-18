3ced133fd88edaca3926dc9a07b9761a29dad0cf
dDee-D-six <dee.hamdee.084@gmail.com>
2023-04-18 17:41:00 +0700

    :bug: fixed from burn usix for mint asix to locked usix and mint asix

--- x/tokenmngr/keeper/msg_server_convert_to_atto.go
+++ x/tokenmngr/keeper/msg_server_convert_to_atto.go
@@ -67,9 +67,9 @@ func (k msgServer) ConvertToAtto(goCtx context.Context, msg *types.MsgConvertToA
 		return nil, sdkerrors.Wrap(types.ErrSendCoinsFromAccountToModule, "Amount of token is too high than current balance due"+err.Error())
 	}
 
-	if err := k.bankKeeper.BurnCoins(ctx, types.ModuleName, convertAmount); err != nil {
-		return nil, err
-	}
+	// if err := k.bankKeeper.BurnCoins(ctx, types.ModuleName, convertAmount); err != nil {
+	// 	return nil, err
+	// }
 
 	attoAmount := sdk.NewCoin("asix", msg.Amount.Amount.MulRaw(1000000000000))
 	if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.NewCoins(attoAmount)); err != nil {
