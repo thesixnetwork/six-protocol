9e8b134999cbd678c76f89da38860a659c69798c
dDee-D-six <dee.hamdee.084@gmail.com>
2023-04-18 17:41:00 +0700

    :bug: fixed from burn usix for mint asix to locked usix and mint asix

--- x/tokenmngr/keeper/msg_server_convert_to_micro.go
+++ x/tokenmngr/keeper/msg_server_convert_to_micro.go
@@ -11,7 +11,7 @@ import (
 func (k msgServer) ConvertToMicro(goCtx context.Context, msg *types.MsgConvertToMicro) (*types.MsgConvertToMicroResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	denom := msg.Amount.Denom
-	convertAmount := sdk.NewCoins(msg.Amount)
+	convertAmount := sdk.NewCoins(msg.Amount) // asix
 
 	// reject if denom is zero
 	if msg.Amount.Amount.IsZero() {
@@ -67,8 +67,14 @@ func (k msgServer) ConvertToMicro(goCtx context.Context, msg *types.MsgConvertTo
 	}
 
 	microSix := sdk.NewCoin("usix", msg.Amount.Amount.QuoRaw(1_000_000_000_000))
-	if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.NewCoins(microSix)); err != nil {
-		return nil, err
+
+	// get the module account balance
+	tokenmngrModuleAccount := k.accountKeeper.GetModuleAddress(types.ModuleName)
+	moduleBalance := k.bankKeeper.GetBalance(ctx, tokenmngrModuleAccount, "usix")
+
+	// check if module account balance is enough to send
+	if moduleBalance.Amount.LT(microSix.Amount) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "module account balance is not enough to send")
 	}
 
 	// send to receiver
