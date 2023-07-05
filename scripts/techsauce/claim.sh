b5313b6db65379c4125a60b7d19f534d63f22fc4
dDee-D-six <dee.hamdee.084@gmail.com>
2023-07-05 19:33:16 +0700

    :white_check_mark: After party redemtion success

--- scripts/techsauce/claim.sh
+++ scripts/techsauce/claim.sh
@@ -4,8 +4,14 @@ if [ -z "$token_id" ]
 then
     read -p "Enter Token ID: " token_id
 fi
+action=$2
+# if action is not provided, ask for it
+if [ -z "$action" ]
+then
+    read -p "Enter Action: " action
+fi
 uuid=$(uuidgen)
 sixd tx nftmngr perform-action-by-nftadmin techsauce.eventname ${token_id} \
-    transform ${uuid} '[]' \
+    ${action} ${uuid} '[]' \
     --from alice --gas auto --gas-adjustment 1.5 --gas-prices 1.25usix -y \
     --node http://localhost:26657 --chain-id testnet
