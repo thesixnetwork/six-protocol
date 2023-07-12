d4ab2398b136e494f5e413b4c1acf10c17d9c7e1
dDee-D-six <dee.hamdee.084@gmail.com>
2023-07-12 11:52:09 +0700

    :art: format json of schema and data

--- scripts/techsauce/redeem.sh
+++ scripts/techsauce/redeem.sh
@@ -1,15 +1,13 @@
 token_id=$1
-if [ -z "$token_id" ]
-then
+if [ -z "$token_id" ]; then
     read -p "Enter Token ID: " token_id
 fi
 stock=$2
-if [ -z "$stock" ]
-then
+if [ -z "$stock" ]; then
     read -p "Enter Stock: " stock
 fi
 uuid=$(uuidgen)
 params='[{"name":"stock","value":"'$stock'"}]'
 sixd tx nftmngr perform-action-by-nftadmin techsauce.eventname ${token_id} \
     redeem_prize ${uuid} ${params} \
-    --from alice --gas auto --gas-adjustment 1.5 --gas-prices 1.25usix -y --chain-id testnet 
+    --from alice --gas auto --gas-adjustment 1.5 --gas-prices 1.25usix -y --chain-id testnet
