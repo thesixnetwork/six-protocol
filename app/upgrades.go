83c38d9a361142db759ad258aa42e78d9ada624c
dDee-D-six <dee.hamdee.084@gmail.com>
2023-09-08 19:16:17 +0700

    :white_check_mark: migration rollback complete

--- app/upgrades.go
+++ app/upgrades.go
@@ -16,7 +16,7 @@ const UpgradeName = "v3.1.2-c"
 func (app *App) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(UpgradeName, func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 
-		// app.MigrationFromV1ToV2Handlers(ctx) == > for mainnet v3.1.3
+		app.RollbackFromV2toV1(ctx)
 		return app.mm.RunMigrations(ctx, app.configurator, vm)
 	})
 }
