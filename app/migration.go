6a4c59e042f74055f767ef658ead5a6775d3116d
dDeeDev <dee.hamdee.084@gmail.com>
2024-09-26 14:49:36 +0700

    :truck: migration evm params

--- app/migration.go
+++ app/migration.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/thesixnetwork/six-protocol/precompiles"
 )
 
 func (app *App) MigrateParam(ctx sdk.Context) {
@@ -10,15 +9,5 @@ func (app *App) MigrateParam(ctx sdk.Context) {
 	migrateParam := currentParam
 	migrateParam.AllowUnprotectedTxs = true
 	app.EVMKeeper.SetParams(ctx, migrateParam)
-
-	if err := precompiles.InitializePrecompiles(
-		false,
-		app.appCodec,
-		app.BankKeeper,
-		app.AccountKeeper,
-		app.NftmngrKeeper,
-	); err != nil {
-		panic(err)
-	}
 
 }
