29e0167a793c846f3eff2d871ceb6cc95a9aaefb
dDeeDev <dee.hamdee.084@gmail.com>
2024-11-12 15:18:20 +0700

    call bank precompil succesfull

--- precompiles/bank/Bank.sol
+++ precompiles/bank/Bank.sol
@@ -3,51 +3,31 @@ pragma solidity ^0.8.0;
 
 address constant BANK_PRECOMPILE_ADDRESS = 0x0000000000000000000000000000000000001001;
 
-IBank constant BANK_CONTRACT = IBank(
-    BANK_PRECOMPILE_ADDRESS
-);
+IBank constant BANK_CONTRACT = IBank(BANK_PRECOMPILE_ADDRESS);
 
 interface IBank {
     // Transactions
-    function send(
-        address fromAddress,
-        address toAddress,
-        string memory denom,
-        uint256 amount
-    ) external returns (bool success);
+    function send(address fromAddress, address toAddress, string memory denom, uint256 amount)
+        external
+        returns (bool success);
 
-    function sendNative(
-        string memory toNativeAddress
-    ) payable external returns (bool success);
+    function sendNative(string memory toNativeAddress) external payable returns (bool success);
 
     // Queries
-    function balance(
-        address acc,
-        string memory denom
-    ) external view returns (uint256 amount);
+    function balance(address acc, string memory denom) external view returns (uint256 amount);
 
     struct Coin {
         uint256 amount;
         string denom;
     }
 
-    function all_balances(
-        address acc
-    ) external view returns (Coin[] memory response);
+    function all_balances(address acc) external view returns (Coin[] memory response);
 
-    function name(
-        string memory denom
-    ) external view returns (string memory response);
+    function name(string memory denom) external view returns (string memory response);
 
-    function symbol(
-        string memory denom
-    ) external view returns (string memory response);
+    function symbol(string memory denom) external view returns (string memory response);
 
-    function decimals(
-        string memory denom
-    ) external view returns (uint8 response);
+    function decimals(string memory denom) external view returns (uint8 response);
 
-    function supply(
-        string memory denom
-    ) external view returns (uint256 response);
+    function supply(string memory denom) external view returns (uint256 response);
 }
