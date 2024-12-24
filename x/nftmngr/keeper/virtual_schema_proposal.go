376948ce855a5c0425d912cb45068c25d3bbe699
dDeeDev <dee.hamdee.084@gmail.com>
2024-12-24 17:23:39 +0700

    chore(nftmngr): clean up code by removing unnecessary blank lines

--- x/nftmngr/keeper/virtual_schema_proposal.go
+++ x/nftmngr/keeper/virtual_schema_proposal.go
@@ -158,6 +158,61 @@ func (k Keeper) IterateActiveProposal(ctx sdk.Context, endTime time.Time, cb fun
 	}
 }
 
+// SetInactiveVirtualSchemaProposal set a specific inactiveVirtualSchemaProposal in the store from its index
+func (k Keeper) SetInactiveVirtualSchemaProposal(ctx sdk.Context, inactiveVirtualSchemaProposal types.InactiveVirtualSchemaProposal) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.InactiveVirtualSchemaProposalKeyPrefix))
+	b := k.cdc.MustMarshal(&inactiveVirtualSchemaProposal)
+	store.Set(types.InactiveVirtualSchemaProposalKey(
+		inactiveVirtualSchemaProposal.Id,
+	), b)
+}
+
+// GetInactiveVirtualSchemaProposal returns a inactiveVirtualSchemaProposal from its index
+func (k Keeper) GetInactiveVirtualSchemaProposal(
+	ctx sdk.Context,
+	index string,
+) (val types.InactiveVirtualSchemaProposal, found bool) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.InactiveVirtualSchemaProposalKeyPrefix))
+
+	b := store.Get(types.InactiveVirtualSchemaProposalKey(
+		index,
+	))
+	if b == nil {
+		return val, false
+	}
+
+	k.cdc.MustUnmarshal(b, &val)
+	return val, true
+}
+
+// RemoveInactiveVirtualSchemaProposal removes a inactiveVirtualSchemaProposal from the store
+func (k Keeper) RemoveInactiveVirtualSchemaProposal(
+	ctx sdk.Context,
+	index string,
+) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.InactiveVirtualSchemaProposalKeyPrefix))
+	store.Delete(types.InactiveVirtualSchemaProposalKey(
+		index,
+	))
+}
+
+// GetAllInactiveVirtualSchemaProposal returns all inactiveVirtualSchemaProposal
+func (k Keeper) GetAllInactiveVirtualSchemaProposal(ctx sdk.Context) (list []types.InactiveVirtualSchemaProposal) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.InactiveVirtualSchemaProposalKeyPrefix))
+	iterator := sdk.KVStorePrefixIterator(store, []byte{})
+
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		var val types.InactiveVirtualSchemaProposal
+		k.cdc.MustUnmarshal(iterator.Value(), &val)
+		list = append(list, val)
+	}
+
+	return
+}
+
+
 func (k Keeper) ActiveProposalQueryIterator(ctx sdk.Context, endTime time.Time) sdk.Iterator {
 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.ActiveVirtualSchemaProposalKeyPrefix))
 	iterator := sdk.KVStorePrefixIterator(store, []byte{})
