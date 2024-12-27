c37f8bb254708f355298ab7fefe59ef0d0b91efd
dDeeDev <dee.hamdee.084@gmail.com>
2024-12-27 11:47:31 +0700

    chore(nftmngr): reduce files

---
+++ x/nftmngr/types/message_owner.go
@@ -0,0 +1,89 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+const TypeMsgChangeOrgOwner = "chage_org_owner"
+
+var _ sdk.Msg = &MsgChangeOrgOwner{}
+
+func NewMsgChangeOrgOwner(creator string, orgName string, toNewOwner string) *MsgChangeOrgOwner {
+	return &MsgChangeOrgOwner{
+		Creator:    creator,
+		OrgName:    orgName,
+		ToNewOwner: toNewOwner,
+	}
+}
+
+func (msg *MsgChangeOrgOwner) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgChangeOrgOwner) Type() string {
+	return TypeMsgChangeOrgOwner
+}
+
+func (msg *MsgChangeOrgOwner) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgChangeOrgOwner) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgChangeOrgOwner) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+
+const TypeMsgChangeSchemaOwner = "change_schema_owner"
+
+var _ sdk.Msg = &MsgChangeSchemaOwner{}
+
+func NewMsgChangeSchemaOwner(creator string, nftSchemaCode string, newOwner string) *MsgChangeSchemaOwner {
+	return &MsgChangeSchemaOwner{
+		Creator:       creator,
+		NftSchemaCode: nftSchemaCode,
+		NewOwner:      newOwner,
+	}
+}
+
+func (msg *MsgChangeSchemaOwner) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgChangeSchemaOwner) Type() string {
+	return TypeMsgChangeSchemaOwner
+}
+
+func (msg *MsgChangeSchemaOwner) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgChangeSchemaOwner) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgChangeSchemaOwner) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
---
+++ x/nftmngr/types/message_schema.go
@@ -0,0 +1,332 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+const TypeMsgSetUriRetrievalMethod = "set_uri_retrieval_method"
+
+var _ sdk.Msg = &MsgSetUriRetrievalMethod{}
+
+func NewMsgSetUriRetrievalMethod(creator string, schemaCode string, newMethod int32) *MsgSetUriRetrievalMethod {
+	return &MsgSetUriRetrievalMethod{
+		Creator:    creator,
+		SchemaCode: schemaCode,
+		NewMethod:  newMethod,
+	}
+}
+
+func (msg *MsgSetUriRetrievalMethod) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgSetUriRetrievalMethod) Type() string {
+	return TypeMsgSetUriRetrievalMethod
+}
+
+func (msg *MsgSetUriRetrievalMethod) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgSetUriRetrievalMethod) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgSetUriRetrievalMethod) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgCreateNFTSchema = "create_nft_schema"
+
+var _ sdk.Msg = &MsgCreateNFTSchema{}
+
+func NewMsgCreateNFTSchema(creator string, nftSchemaBase64 string) *MsgCreateNFTSchema {
+	return &MsgCreateNFTSchema{
+		Creator:         creator,
+		NftSchemaBase64: nftSchemaBase64,
+	}
+}
+
+func (msg *MsgCreateNFTSchema) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgCreateNFTSchema) Type() string {
+	return TypeMsgCreateNFTSchema
+}
+
+func (msg *MsgCreateNFTSchema) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgCreateNFTSchema) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgCreateNFTSchema) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgDisableVirtualSchemaProposal = "disable_virtual_schema_proposal"
+
+var _ sdk.Msg = &MsgDisableVirtualSchemaProposal{}
+
+func NewMsgDisableVirtualSchemaProposal(creator string, virtualNftSchemaCode string) *MsgDisableVirtualSchemaProposal {
+	return &MsgDisableVirtualSchemaProposal{
+		Creator:              creator,
+		VirtualNftSchemaCode: virtualNftSchemaCode,
+	}
+}
+
+func (msg *MsgDisableVirtualSchemaProposal) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgDisableVirtualSchemaProposal) Type() string {
+	return TypeMsgDisableVirtualSchemaProposal
+}
+
+func (msg *MsgDisableVirtualSchemaProposal) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgDisableVirtualSchemaProposal) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgDisableVirtualSchemaProposal) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgSetBaseUri = "set_base_uri"
+
+var _ sdk.Msg = &MsgSetBaseUri{}
+
+func NewMsgSetBaseUri(creator string, code string, newBaseUri string) *MsgSetBaseUri {
+	return &MsgSetBaseUri{
+		Creator:    creator,
+		Code:       code,
+		NewBaseUri: newBaseUri,
+	}
+}
+
+func (msg *MsgSetBaseUri) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgSetBaseUri) Type() string {
+	return TypeMsgSetBaseUri
+}
+
+func (msg *MsgSetBaseUri) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgSetBaseUri) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgSetBaseUri) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgSetMetadataFormat = "set_metadata_format"
+
+var _ sdk.Msg = &MsgSetMetadataFormat{}
+
+func NewMsgSetMetadataFormat(creator string, schemaCode string, newFormat string) *MsgSetMetadataFormat {
+	return &MsgSetMetadataFormat{
+		Creator:    creator,
+		SchemaCode: schemaCode,
+		NewFormat:  newFormat,
+	}
+}
+
+func (msg *MsgSetMetadataFormat) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgSetMetadataFormat) Type() string {
+	return TypeMsgSetMetadataFormat
+}
+
+func (msg *MsgSetMetadataFormat) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgSetMetadataFormat) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgSetMetadataFormat) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgSetMintauth = "set_mintauth"
+
+var _ sdk.Msg = &MsgSetMintauth{}
+
+func NewMsgSetMintauth(creator string, nftSchemaCode string, authorizeTo AuthorizeTo) *MsgSetMintauth {
+	return &MsgSetMintauth{
+		Creator:       creator,
+		NftSchemaCode: nftSchemaCode,
+		AuthorizeTo:   authorizeTo,
+	}
+}
+
+func (msg *MsgSetMintauth) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgSetMintauth) Type() string {
+	return TypeMsgSetMintauth
+}
+
+func (msg *MsgSetMintauth) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgSetMintauth) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgSetMintauth) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgSetOriginChain = "set_origin_chain"
+
+var _ sdk.Msg = &MsgSetOriginChain{}
+
+func NewMsgSetOriginChain(creator string, schemaCode string, newOriginChain string) *MsgSetOriginChain {
+	return &MsgSetOriginChain{
+		Creator:        creator,
+		SchemaCode:     schemaCode,
+		NewOriginChain: newOriginChain,
+	}
+}
+
+func (msg *MsgSetOriginChain) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgSetOriginChain) Type() string {
+	return TypeMsgSetOriginChain
+}
+
+func (msg *MsgSetOriginChain) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgSetOriginChain) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgSetOriginChain) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgSetOriginContract = "set_origin_contract"
+
+var _ sdk.Msg = &MsgSetOriginContract{}
+
+func NewMsgSetOriginContract(creator string, schemaCode string, newContractAddress string) *MsgSetOriginContract {
+	return &MsgSetOriginContract{
+		Creator:            creator,
+		SchemaCode:         schemaCode,
+		NewContractAddress: newContractAddress,
+	}
+}
+
+func (msg *MsgSetOriginContract) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgSetOriginContract) Type() string {
+	return TypeMsgSetOriginContract
+}
+
+func (msg *MsgSetOriginContract) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgSetOriginContract) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgSetOriginContract) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
