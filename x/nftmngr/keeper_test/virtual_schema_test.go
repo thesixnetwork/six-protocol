19fa2a014df2dc58464781807b7bd6c8293dc9a1
dDeeDev <dee.hamdee.084@gmail.com>
2024-12-27 11:47:31 +0700

    chore(nftmngr): reduce files

--- x/nftmngr/keeper_test/virtual_schema_test.go
+++ x/nftmngr/keeper_test/virtual_schema_test.go
@@ -5,7 +5,10 @@ import (
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	keepertest "github.com/thesixnetwork/six-protocol/testutil/keeper"
 	"github.com/thesixnetwork/six-protocol/testutil/nullify"
@@ -62,4 +65,159 @@ func TestVirtualSchemaGetAll(t *testing.T) {
 		nullify.Fill(items),
 		nullify.Fill(keeper.GetAllVirtualSchema(ctx)),
 	)
+}
+
+func createNDisableVirtualSchema(keeper *keeper.Keeper, ctx sdk.Context, n int) []types.DisableVirtualSchema {
+	items := make([]types.DisableVirtualSchema, n)
+	for i := range items {
+		items[i].Id = strconv.Itoa(i)
+
+		keeper.SetDisableVirtualSchema(ctx, items[i])
+	}
+	return items
+}
+
+func TestDisableVirtualSchemaGet(t *testing.T) {
+	keeper, ctx := keepertest.NftmngrKeeper(t)
+	items := createNDisableVirtualSchema(keeper, ctx, 10)
+	for _, item := range items {
+		rst, found := keeper.GetDisableVirtualSchema(ctx,
+			item.Id,
+		)
+		require.True(t, found)
+		require.Equal(t,
+			nullify.Fill(&item),
+			nullify.Fill(&rst),
+		)
+	}
+}
+
+func TestDisableVirtualSchemaRemove(t *testing.T) {
+	keeper, ctx := keepertest.NftmngrKeeper(t)
+	items := createNDisableVirtualSchema(keeper, ctx, 10)
+	for _, item := range items {
+		keeper.RemoveDisableVirtualSchema(ctx,
+			item.Id,
+		)
+		_, found := keeper.GetDisableVirtualSchema(ctx,
+			item.Id,
+		)
+		require.False(t, found)
+	}
+}
+
+func TestDisableVirtualSchemaGetAll(t *testing.T) {
+	keeper, ctx := keepertest.NftmngrKeeper(t)
+	items := createNDisableVirtualSchema(keeper, ctx, 10)
+	require.ElementsMatch(t,
+		nullify.Fill(items),
+		nullify.Fill(keeper.GetAllDisableVirtualSchema(ctx)),
+	)
+}
+
+func TestDisableVirtualSchemaQuerySingle(t *testing.T) {
+	keeper, ctx := keepertest.NftmngrKeeper(t)
+	wctx := sdk.WrapSDKContext(ctx)
+	msgs := createNDisableVirtualSchema(keeper, ctx, 2)
+	for _, tc := range []struct {
+		desc     string
+		request  *types.QueryGetDisableVirtualSchemaRequest
+		response *types.QueryGetDisableVirtualSchemaResponse
+		err      error
+	}{
+		{
+			desc: "First",
+			request: &types.QueryGetDisableVirtualSchemaRequest{
+				Index: msgs[0].Id,
+			},
+			response: &types.QueryGetDisableVirtualSchemaResponse{DisableVirtualSchema: msgs[0]},
+		},
+		{
+			desc: "Second",
+			request: &types.QueryGetDisableVirtualSchemaRequest{
+				Index: msgs[1].Id,
+			},
+			response: &types.QueryGetDisableVirtualSchemaResponse{DisableVirtualSchema: msgs[1]},
+		},
+		{
+			desc: "KeyNotFound",
+			request: &types.QueryGetDisableVirtualSchemaRequest{
+				Index: strconv.Itoa(100000),
+			},
+			err: status.Error(codes.NotFound, "not found"),
+		},
+		{
+			desc: "InvalidRequest",
+			err:  status.Error(codes.InvalidArgument, "invalid request"),
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			response, err := keeper.DisableVirtualSchema(wctx, tc.request)
+			if tc.err != nil {
+				require.ErrorIs(t, err, tc.err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t,
+					nullify.Fill(tc.response),
+					nullify.Fill(response),
+				)
+			}
+		})
+	}
+}
+
+func TestDisableVirtualSchemaQueryPaginated(t *testing.T) {
+	keeper, ctx := keepertest.NftmngrKeeper(t)
+	wctx := sdk.WrapSDKContext(ctx)
+	msgs := createNDisableVirtualSchema(keeper, ctx, 5)
+
+	request := func(next []byte, offset, limit uint64, total bool) *types.QueryAllDisableVirtualSchemaRequest {
+		return &types.QueryAllDisableVirtualSchemaRequest{
+			Pagination: &query.PageRequest{
+				Key:        next,
+				Offset:     offset,
+				Limit:      limit,
+				CountTotal: total,
+			},
+		}
+	}
+	t.Run("ByOffset", func(t *testing.T) {
+		step := 2
+		for i := 0; i < len(msgs); i += step {
+			resp, err := keeper.DisableVirtualSchemaAll(wctx, request(nil, uint64(i), uint64(step), false))
+			require.NoError(t, err)
+			require.LessOrEqual(t, len(resp.DisableVirtualSchema), step)
+			require.Subset(t,
+				nullify.Fill(msgs),
+				nullify.Fill(resp.DisableVirtualSchema),
+			)
+		}
+	})
+	t.Run("ByKey", func(t *testing.T) {
+		step := 2
+		var next []byte
+		for i := 0; i < len(msgs); i += step {
+			resp, err := keeper.DisableVirtualSchemaAll(wctx, request(next, 0, uint64(step), false))
+			require.NoError(t, err)
+			require.LessOrEqual(t, len(resp.DisableVirtualSchema), step)
+			require.Subset(t,
+				nullify.Fill(msgs),
+				nullify.Fill(resp.DisableVirtualSchema),
+			)
+			next = resp.Pagination.NextKey
+		}
+	})
+	t.Run("Total", func(t *testing.T) {
+		resp, err := keeper.DisableVirtualSchemaAll(wctx, request(nil, 0, 0, true))
+		require.NoError(t, err)
+		require.Equal(t, len(msgs), int(resp.Pagination.Total))
+		require.ElementsMatch(t,
+			nullify.Fill(msgs),
+			nullify.Fill(resp.DisableVirtualSchema),
+		)
+	})
+	t.Run("InvalidRequest", func(t *testing.T) {
+		_, err := keeper.DisableVirtualSchemaAll(wctx, nil)
+		require.ErrorIs(t, err, status.Error(codes.InvalidArgument, "invalid request"))
+	})
 }
---
+++ x/nftmngr/types/message_action.go
@@ -0,0 +1,269 @@
+package types
+
+import (
+	"encoding/json"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+const TypeMsgAddAction = "add_action"
+
+var _ sdk.Msg = &MsgAddAction{}
+
+func NewMsgAddAction(creator string, code string, base64NewAction string) *MsgAddAction {
+	return &MsgAddAction{
+		Creator:         creator,
+		Code:            code,
+		Base64NewAction: base64NewAction,
+	}
+}
+
+func (msg *MsgAddAction) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgAddAction) Type() string {
+	return TypeMsgAddAction
+}
+
+func (msg *MsgAddAction) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgAddAction) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgAddAction) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgUpdateAction = "update_action"
+
+var _ sdk.Msg = &MsgUpdateAction{}
+
+func NewMsgUpdateAction(creator string, nftSchemaCode string, base64UpdateAction string) *MsgUpdateAction {
+	return &MsgUpdateAction{
+		Creator:            creator,
+		NftSchemaCode:      nftSchemaCode,
+		Base64UpdateAction: base64UpdateAction,
+	}
+}
+
+func (msg *MsgUpdateAction) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgUpdateAction) Type() string {
+	return TypeMsgUpdateAction
+}
+
+func (msg *MsgUpdateAction) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgUpdateAction) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgUpdateAction) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgToggleAction = "toggle_action"
+
+var _ sdk.Msg = &MsgToggleAction{}
+
+func NewMsgToggleAction(creator string, code string, action string, status bool) *MsgToggleAction {
+	return &MsgToggleAction{
+		Creator: creator,
+		Code:    code,
+		Action:  action,
+		Status:  status,
+	}
+}
+
+func (msg *MsgToggleAction) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgToggleAction) Type() string {
+	return TypeMsgToggleAction
+}
+
+func (msg *MsgToggleAction) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgToggleAction) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgToggleAction) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgCreateMultiMetadata = "create_multi_metadata"
+
+var _ sdk.Msg = &MsgCreateMultiMetadata{}
+
+func NewMsgCreateMultiMetadata(creator string, nftSchemaCode string, tokenId []string, base64NFTData string) *MsgCreateMultiMetadata {
+	return &MsgCreateMultiMetadata{
+		Creator:       creator,
+		NftSchemaCode: nftSchemaCode,
+		TokenId:       tokenId,
+		Base64NFTData: base64NFTData,
+	}
+}
+
+func (msg *MsgCreateMultiMetadata) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgCreateMultiMetadata) Type() string {
+	return TypeMsgCreateMultiMetadata
+}
+
+func (msg *MsgCreateMultiMetadata) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgCreateMultiMetadata) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgCreateMultiMetadata) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgPerformActionByAdmin = "perform_action_by_admin"
+
+var _ sdk.Msg = &MsgPerformActionByAdmin{}
+
+func NewMsgPerformActionByAdmin(creator, nftSchemaCode, tokenId, action, actionPrams, refId string) *MsgPerformActionByAdmin {
+	// string to json object of  []*ActionParameter
+	var actionPrams_ []*ActionParameter
+	err := json.Unmarshal([]byte(actionPrams), &actionPrams_)
+	if err != nil {
+		panic(err)
+	}
+
+	return &MsgPerformActionByAdmin{
+		Creator:       creator,
+		NftSchemaCode: nftSchemaCode,
+		TokenId:       tokenId,
+		Action:        action,
+		Parameters:    actionPrams_,
+		RefId:         refId,
+	}
+}
+
+func (msg *MsgPerformActionByAdmin) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgPerformActionByAdmin) Type() string {
+	return TypeMsgPerformActionByAdmin
+}
+
+func (msg *MsgPerformActionByAdmin) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgPerformActionByAdmin) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgPerformActionByAdmin) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
+
+const TypeMsgPerformVirtualAction = "perform_virtual_action"
+
+var _ sdk.Msg = &MsgPerformVirtualAction{}
+
+func NewMsgPerformVirtualAction(creator, nftSchemaName string, tokenIdsInput []*TokenIdMap, actionName string, paramInput []*ActionParameter, refId string) *MsgPerformVirtualAction {
+	return &MsgPerformVirtualAction{
+		Creator:       creator,
+		NftSchemaName: nftSchemaName,
+		TokenIdMap:    tokenIdsInput,
+		Action:        actionName,
+		Parameters:    paramInput,
+		RefId:         refId,
+	}
+}
+
+func (msg *MsgPerformVirtualAction) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgPerformVirtualAction) Type() string {
+	return TypeMsgPerformVirtualAction
+}
+
+func (msg *MsgPerformVirtualAction) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgPerformVirtualAction) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgPerformVirtualAction) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
