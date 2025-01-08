8a3a7dedd4043bcd5e727b1a2b28021434bed2e5
dDeeDev <dee.hamdee.084@gmail.com>
2025-01-08 14:35:10 +0700

    feat(nftmngr): list all active proposal

---
+++ x/nftmngr/client/cli/query_list_active_proposal.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+	"github.com/thesixnetwork/six-protocol/x/nftmngr/types"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdListActiveProposal() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-active-proposal",
+		Short: "Query listActiveProposal",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryListActiveProposalRequest{}
+
+			res, err := queryClient.ListActiveProposal(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
---
+++ x/nftmngr/keeper/grpc_query_list_active_proposal.go
@@ -0,0 +1,35 @@
+package keeper
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/thesixnetwork/six-protocol/x/nftmngr/types"
+)
+
+// TODO:: Add pagination on next release
+func (k Keeper) ListActiveProposal(goCtx context.Context, req *types.QueryListActiveProposalRequest) (*types.QueryListActiveProposalResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	var virtualSchemaProposals []types.VirtualSchemaProposal
+	listActiveProposal := k.GetAllActiveVirtualSchemaProposal(ctx)
+
+	for _, virtualSchemaProposal := range listActiveProposal {
+		proposal, found := k.GetVirtualSchemaProposal(ctx, virtualSchemaProposal.Id)
+		if !found {
+			return nil, status.Error(codes.NotFound, "not found")
+		}
+		virtualSchemaProposals = append(virtualSchemaProposals, proposal)
+	}
+
+	return &types.QueryListActiveProposalResponse{
+		VirtualSchemaProposal: virtualSchemaProposals,
+	}, nil
+}
