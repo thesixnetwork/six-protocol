5de7c921f97ce73126fb2ac5dd390c2ce533aa5b
dDeeDev <dee.hamdee.084@gmail.com>
2025-01-08 14:35:10 +0700

    feat(nftmngr): list all active proposal

--- x/nftmngr/client/cli/query.go
+++ x/nftmngr/client/cli/query.go
@@ -53,6 +53,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdShowVirtualSchema())
 	cmd.AddCommand(CmdShowVirtualSchemaProposal())
 	cmd.AddCommand(CmdListVirtualSchemaProposal())
+	cmd.AddCommand(CmdListActiveProposal())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
--- x/nftmngr/types/query.pb.gw.go
+++ x/nftmngr/types/query.pb.gw.go
@@ -1507,6 +1507,24 @@ func local_request_Query_VirtualSchemaProposalAll_0(ctx context.Context, marshal
 
 }
 
+func request_Query_ListActiveProposal_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryListActiveProposalRequest
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.ListActiveProposal(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Query_ListActiveProposal_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryListActiveProposalRequest
+	var metadata runtime.ServerMetadata
+
+	msg, err := server.ListActiveProposal(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterQueryHandlerServer registers the http handlers for service Query to "mux".
 // UnaryRPC     :call QueryServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -2226,6 +2244,29 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 
 	})
 
+	mux.Handle("GET", pattern_Query_ListActiveProposal_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Query_ListActiveProposal_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Query_ListActiveProposal_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -2887,6 +2928,26 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("GET", pattern_Query_ListActiveProposal_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Query_ListActiveProposal_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Query_ListActiveProposal_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -2952,6 +3013,8 @@ var (
 	pattern_Query_VirtualSchemaProposal_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"thesixnetwork", "sixnft", "nftmngr", "virtual_schema_proposal", "index"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Query_VirtualSchemaProposalAll_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"thesixnetwork", "sixnft", "nftmngr", "virtual_schema_proposal"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_Query_ListActiveProposal_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"thesixnetwork", "six-protocol", "nftmngr", "list_active_proposal"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -3016,4 +3079,6 @@ var (
 	forward_Query_VirtualSchemaProposal_0 = runtime.ForwardResponseMessage
 
 	forward_Query_VirtualSchemaProposalAll_0 = runtime.ForwardResponseMessage
+
+	forward_Query_ListActiveProposal_0 = runtime.ForwardResponseMessage
 )
