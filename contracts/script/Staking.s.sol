f6ad0b4021d87b20b97df481befd71fa2ef5d13f
dDeeDev <dee.hamdee.084@gmail.com>
2025-04-08 16:49:14 +0700

    remove query from foundry

--- contracts/script/Staking.s.sol
+++ contracts/script/Staking.s.sol
@@ -4,44 +4,6 @@ pragma solidity ^0.8.20;
 import "forge-std/Script.sol";
 import {IStaking, STAKING_PRECOMPILE_ADDRESS} from "../src/precompiles/IStaking.sol";
 
-contract StakeScript is Script {
-    address ownerAddress;
-    uint64 currentNonce;
-
-    function setUp() public {
-        ownerAddress = vm.envAddress("OWNER");
-    }
-
-    function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
-        address contractAddress = STAKING_PRECOMPILE_ADDRESS;
-        // Using the interface approach (recommended)
-        IStaking stakeContract = IStaking(contractAddress);
-        IStaking.Delegation memory delegationResult = stakeContract.delegation(
-            "6x1t3p2vzd7w036ahxf4kefsc9sn24pvlqphcuauv",
-            "6xvaloper1t3p2vzd7w036ahxf4kefsc9sn24pvlqpmk79jh"
-        );
-
-        // Display results using the interface call
-        console.log("=== Delegation Information ===");
-        console.log("Amount:", delegationResult.balance.amount);
-        console.log("Denom:", delegationResult.balance.denom);
-        console.log(
-            "Delegator:",
-            delegationResult.delegation.delegator_address
-        );
-        console.log(
-            "Validator:",
-            delegationResult.delegation.validator_address
-        );
-        console.log("Shares:", delegationResult.delegation.shares);
-        console.log("Decimals:", delegationResult.delegation.decimals);
-
-        vm.stopBroadcast();
-    }
-}
-
 contract delegateScript is Script {
     address ownerAddress;
     uint64 currentNonce;
