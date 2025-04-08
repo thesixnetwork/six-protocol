c6f6c809db19094a2903a74b2fe8a89efda782a6
dDeeDev <dee.hamdee.084@gmail.com>
2025-04-08 16:44:40 +0700

    feat: undelegate success

--- contracts/script/Staking.s.sol
+++ contracts/script/Staking.s.sol
@@ -70,3 +70,32 @@ contract delegateScript is Script {
         vm.stopBroadcast();
     }
 }
+
+contract undelegateScript is Script {
+    address ownerAddress;
+    uint64 currentNonce;
+
+    function setUp() public {
+        ownerAddress = vm.envAddress("OWNER");
+    }
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        address contractAddress = STAKING_PRECOMPILE_ADDRESS;
+        // Execute the transaction
+        (bool success, bytes memory result) = contractAddress.call(
+            abi.encodeWithSignature(
+                "undelegate(string,uint256)",
+                "6xvaloper1t3p2vzd7w036ahxf4kefsc9sn24pvlqpmk79jh",
+                20000 * 1e18
+            )
+        );
+        require(success, "Transaction failed");
+
+        // Log the success message
+        console.log("Transfer success!");
+        console.log(string(result));
+        vm.stopBroadcast();
+    }
+}
