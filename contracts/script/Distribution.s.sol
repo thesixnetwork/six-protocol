dcea287047dc8a98e33a97a0c60898985adfda5e
dDeeDev <dee.hamdee.084@gmail.com>
2025-04-11 15:37:42 +0700

    feat: convert usix to asix implemted \n todo: test convert usix to asix \n todo: test case for staking and distribution

--- contracts/script/Distribution.s.sol
+++ contracts/script/Distribution.s.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import {IStaking, STAKING_PRECOMPILE_ADDRESS} from "../src/precompiles/IStaking.sol";
+import {IDistr, DISTR_PRECOMPILE_ADDRESS} from "../src/precompiles/IDistribution.sol";
 
-contract delegateScript is Script {
+contract SetAddressScript is Script {
     address ownerAddress;
     uint64 currentNonce;
 
@@ -15,25 +15,23 @@ contract delegateScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        address contractAddress = STAKING_PRECOMPILE_ADDRESS;
+        address contractAddress = DISTR_PRECOMPILE_ADDRESS;
         // Execute the transaction
-        (bool success, bytes memory result) = contractAddress.call(
+        (bool success, ) = contractAddress.call(
             abi.encodeWithSignature(
-                "delegate(string,uint256)",
-                "6xvaloper1t3p2vzd7w036ahxf4kefsc9sn24pvlqpmk79jh",
-                10000 * 1e18
+                "setWithdrawAddress(string)",
+                "6x1kch0sdjr5tuvjh0h3a55c6l5sr6m0phjeag9f2"
             )
         );
         require(success, "Transaction failed");
 
         // Log the success message
         console.log("Transfer success!");
-        console.log(string(result));
         vm.stopBroadcast();
     }
 }
 
-contract undelegateScript is Script {
+contract WithDrawScript is Script {
     address ownerAddress;
     uint64 currentNonce;
 
@@ -44,20 +42,18 @@ contract undelegateScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        address contractAddress = STAKING_PRECOMPILE_ADDRESS;
+        address contractAddress = DISTR_PRECOMPILE_ADDRESS;
         // Execute the transaction
-        (bool success, bytes memory result) = contractAddress.call(
+        (bool success, ) = contractAddress.call(
             abi.encodeWithSignature(
-                "undelegate(string,uint256)",
-                "6xvaloper1t3p2vzd7w036ahxf4kefsc9sn24pvlqpmk79jh",
-                10000 * 1e18
+                "withdrawRewards(string)",
+                "6xvaloper1t3p2vzd7w036ahxf4kefsc9sn24pvlqpmk79jh"
             )
         );
         require(success, "Transaction failed");
 
         // Log the success message
         console.log("Transfer success!");
-        console.log(string(result));
         vm.stopBroadcast();
     }
 }
---
+++ precompiles/tokenfactory/TokenFactory.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+address constant TOKENFACTORY_PRECOMPILE_ADDRESS = 0x0000000000000000000000000000000000001069;
+
+ITokenFactory constant BRIDGE_CONTRACT = ITokenFactory(TOKENFACTORY_PRECOMPILE_ADDRESS);
+
+interface ITokenFactory {
+  function transferToCosmos(string memory dst, uint256 amount) external returns (bool success);
+
+  function unwrapStakeToken(uint256 amount) external returns (bool success);
+}
