e85f2592e25ecca7754d6a176f13dd55963b4fad
dDeeDev <dee.hamdee.084@gmail.com>
2025-06-06 12:22:55 +0700

    fix: contract

--- contracts/src/ERC721/WhaleGate.sol
+++ contracts/src/ERC721/WhaleGate.sol
@@ -1,50 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "openzeppelin-contracts/token/ERC721/ERC721.sol";
-import "openzeppelin-contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "openzeppelin-contracts/access/Ownable.sol";
-import "openzeppelin-contracts/utils/Counters.sol";
+import {ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "openzeppelin-contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 
 contract WhaleGate is ERC721, ERC721Enumerable, Ownable {
-    using Counters for Counters.Counter;
-    string public baseURI = "";
+  string public baseURI = "";
+  uint256 private _nextTokenId;
 
-    Counters.Counter private _tokenIdCounter;
+  constructor(address initialOwner) ERC721("WHALEGATE", "WG") Ownable(initialOwner) {
+    _nextTokenId = 1;
+  }
 
-    constructor() ERC721("WHALEGATE", "WG") {
-        _tokenIdCounter._value = 1;
-    }
+  function setBaseURI(string memory _newUri) external {
+    baseURI = _newUri;
+  }
 
-    function setBaseURI(string memory _newUri)external {
-        baseURI = _newUri;
-    }
-    
-    function _baseURI() internal view override returns (string memory) {
-        return baseURI;
-    }
+  function _baseURI() internal view override returns (string memory) {
+    return baseURI;
+  }
 
-    function safeMint(address to) public {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-    }
+  function safeMint(address to) public onlyOwner returns (uint256) {
+    uint256 tokenId = _nextTokenId++;
+    _safeMint(to, tokenId);
+    return tokenId;
+  }
 
-    // The following functions are overrides required by Solidity.
+  // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
-    }
+  function _update(
+    address to,
+    uint256 tokenId,
+    address auth
+  ) internal override(ERC721, ERC721Enumerable) returns (address) {
+    return super._update(to, tokenId, auth);
+  }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+  function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
+    super._increaseBalance(account, value);
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
 }
