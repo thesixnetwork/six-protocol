11eeb5434b6a72bbafcc33b75d77a149b304b79a
dDeeDev <dee.hamdee.084@gmail.com>
2025-06-13 11:35:08 +0700

    fix: remove confuse folder name

--- app/ante/evm.go
+++ app/ante/evm.go
@@ -18,7 +18,7 @@ func newMonoEVMAnteHandler(options HandlerOptions) sdk.AnteHandler {
 			options.DistributionKeeper,
 			options.StakingKeeper,
 			options.MaxTxGasWanted,
-			true,
+			options.AllowUnorderedTx,
 		),
 	)
 }
