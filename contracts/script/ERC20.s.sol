5fd08cb2181e785a22b134703ddc56305e283444
ddeedev <dee.hamdee.084@gmail.com>
2025-09-10 15:55:56 +0700

    chore(contract): add script for transfer token erc20:

--- contracts/script/ERC20.s.sol
+++ contracts/script/ERC20.s.sol
@@ -5,24 +5,53 @@ import "forge-std/Script.sol";
 import "../src/ERC20Impl.sol";
 
 contract DeployERC20 is Script {
-    address ownerAddress;
-    uint64 currentNonce;
+  address ownerAddress;
+  uint64 currentNonce;
 
-    function setUp() public {
-        ownerAddress = vm.envAddress("OWNER");
-        currentNonce = vm.getNonce(ownerAddress);
-    }
+  function setUp() public {
+    ownerAddress = vm.envAddress("OWNER");
+    currentNonce = vm.getNonce(ownerAddress);
+  }
 
-    function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
-        ERC20Impl erc20 = new ERC20Impl("TestTokenD", "TTD", 1000000000000000000000000000, ownerAddress);
-        console.log(address(erc20));
-        vm.stopBroadcast();
-    }
+  function run() external {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
+    ERC20Impl erc20 = new ERC20Impl("TestTokenD", "TTD", 1000000000000000000000000000, ownerAddress);
+    console.log(address(erc20));
+    vm.stopBroadcast();
+  }
 
-    function nonceUp() public {
-        vm.setNonce(ownerAddress, currentNonce + uint64(1));
-        currentNonce++;
-    }
+  function nonceUp() public {
+    vm.setNonce(ownerAddress, currentNonce + uint64(1));
+    currentNonce++;
+  }
+}
+
+contract TransferToken is Script {
+  address ownerAddress;
+  uint64 currentNonce;
+  address erc20contractAddress;
+
+  function setUp() public {
+    ownerAddress = vm.envAddress("OWNER");
+    currentNonce = vm.getNonce(ownerAddress);
+    string memory erc20ContractPath = "./broadcast/ERC20.s.sol/666/run-latest.json";
+    string memory erc20ContractInfo = vm.readFile(erc20ContractPath);
+    bytes memory erc20ContractJsonParse = vm.parseJson(erc20ContractInfo, ".transactions[0].contractAddress");
+
+    erc20contractAddress = abi.decode(erc20ContractJsonParse, (address));
+  }
+
+  function run() external {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
+    ERC20Impl erc20 = ERC20Impl(payable(erc20contractAddress));
+    erc20.transfer(ownerAddress, 10 * 1e18);
+    vm.stopBroadcast();
+  }
+
+  function nonceUp() public {
+    vm.setNonce(ownerAddress, currentNonce + uint64(1));
+    currentNonce++;
+  }
 }
