5dfc2ece86fcd20f3c73a97e03fadbe325fc2d83
ddeedev <dee.hamdee.084@gmail.com>
2025-10-02 17:21:53 +0700

    chore(app): add new store key to upgrate keeeper

--- app/upgrades.go
+++ app/upgrades.go
@@ -7,7 +7,15 @@ import (
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	storetypes "cosmossdk.io/store/types"
-	// evmkeeper "github.com/evmos/ethermint/x/evm/keeper"
+	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
+	consensusparamkeeper "github.com/cosmos/cosmos-sdk/x/consensus/keeper"
+	ibcexported "github.com/cosmos/ibc-go/v8/modules/core/exported"
+	icahosttypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/host/types"
+	icacontrollertypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/controller/types"
+	ibcfeetypes "github.com/cosmos/ibc-go/v8/modules/apps/29-fee/types"
+	evidencetypes "cosmossdk.io/x/evidence/types"
+	ratelimittypes "github.com/cosmos/ibc-apps/modules/rate-limiting/v8/types"
+	circuittypes "cosmossdk.io/x/circuit/types"
 )
 
 const UpgradeName = "v4.0.0"
@@ -16,16 +24,27 @@ func (app *App) RegisterUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(UpgradeName, func(ctx context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		return app.ModuleManager.RunMigrations(ctx, app.configurator, vm)
 	})
-}
 
-func (app *App) VersionTrigger() {
+
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
 	if err != nil {
 		panic(fmt.Sprintf("failed to read upgrade info from disk %s", err))
 	}
 	if upgradeInfo.Name == UpgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 		storeUpgrades := storetypes.StoreUpgrades{
-			Added:   []string{},
+			Added:   []string{
+				crisistypes.StoreKey,
+				consensusparamkeeper.StoreKey,
+				circuittypes.StoreKey,
+				evidencetypes.StoreKey,
+
+				ibcexported.StoreKey,
+				icahosttypes.StoreKey,
+				ibcfeetypes.StoreKey,
+				icacontrollertypes.StoreKey,
+
+				ratelimittypes.StoreKey,
+			},
 			Deleted: []string{},
 		}
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
