d41613b14ec604ce47ceff71f5702948e5298f2c
ddeedev <dee.hamdee.084@gmail.com>
2025-10-07 12:12:36 +0700

    chore(nftoracle): add cli for nftoracle

---
+++ x/nftoracle/client/cli/query_action_signer_test.go
@@ -0,0 +1,108 @@
+package cli_test
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	tmcli "github.com/cometbft/cometbft/libs/cli"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	"github.com/stretchr/testify/require"
+
+	"github.com/thesixnetwork/six-protocol/testutil/network"
+	"github.com/thesixnetwork/six-protocol/testutil/nullify"
+	"github.com/thesixnetwork/six-protocol/x/nftoracle/client/cli"
+	"github.com/thesixnetwork/six-protocol/x/nftoracle/types"
+)
+
+// Prevent strconv unused error
+var _ = strconv.IntSize
+
+func networkWithActionSignerObjects(t *testing.T, n int) (*network.Network, []types.ActionSigner) {
+	t.Helper()
+	cfg := network.DefaultConfig()
+	state := types.GenesisState{}
+	require.NoError(t, cfg.Codec.UnmarshalJSON(cfg.GenesisState[types.ModuleName], &state))
+
+	for i := 0; i < n; i++ {
+		actionSigner := types.ActionSigner{
+			ActorAddress: strconv.Itoa(i),
+		}
+		nullify.Fill(&actionSigner)
+		state.ActionSignerList = append(state.ActionSignerList, actionSigner)
+	}
+	buf, err := cfg.Codec.MarshalJSON(&state)
+	require.NoError(t, err)
+	cfg.GenesisState[types.ModuleName] = buf
+	nw, err := network.New(t, cfg)
+	require.NoError(t, err)
+
+	return nw, state.ActionSignerList
+}
+
+func TestListActionSigner(t *testing.T) {
+	net, objs := networkWithActionSignerObjects(t, 5)
+
+	ctx := net.Validators[0].ClientCtx
+	request := func(next []byte, offset, limit uint64, total bool) []string {
+		args := []string{
+			fmt.Sprintf("--%s=json", tmcli.OutputFlag),
+		}
+		if next == nil {
+			args = append(args, fmt.Sprintf("--%s=%d", flags.FlagOffset, offset))
+		} else {
+			args = append(args, fmt.Sprintf("--%s=%s", flags.FlagPageKey, next))
+		}
+		args = append(args, fmt.Sprintf("--%s=%d", flags.FlagLimit, limit))
+		if total {
+			args = append(args, fmt.Sprintf("--%s", flags.FlagCountTotal))
+		}
+		return args
+	}
+	t.Run("ByOffset", func(t *testing.T) {
+		step := 2
+		for i := 0; i < len(objs); i += step {
+			args := request(nil, uint64(i), uint64(step), false)
+			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListActionSigner(), args)
+			require.NoError(t, err)
+			var resp types.QueryAllActionSignerResponse
+			require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
+			require.LessOrEqual(t, len(resp.ActionSigner), step)
+			require.Subset(t,
+				nullify.Fill(objs),
+				nullify.Fill(resp.ActionSigner),
+			)
+		}
+	})
+	t.Run("ByKey", func(t *testing.T) {
+		step := 2
+		var next []byte
+		for i := 0; i < len(objs); i += step {
+			args := request(next, 0, uint64(step), false)
+			out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListActionSigner(), args)
+			require.NoError(t, err)
+			var resp types.QueryAllActionSignerResponse
+			require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
+			require.LessOrEqual(t, len(resp.ActionSigner), step)
+			require.Subset(t,
+				nullify.Fill(objs),
+				nullify.Fill(resp.ActionSigner),
+			)
+			next = resp.Pagination.NextKey
+		}
+	})
+	t.Run("Total", func(t *testing.T) {
+		args := request(nil, 0, uint64(len(objs)), true)
+		out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdListActionSigner(), args)
+		require.NoError(t, err)
+		var resp types.QueryAllActionSignerResponse
+		require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
+		require.NoError(t, err)
+		require.Equal(t, len(objs), int(resp.Pagination.Total))
+		require.ElementsMatch(t,
+			nullify.Fill(objs),
+			nullify.Fill(resp.ActionSigner),
+		)
+	})
+}
