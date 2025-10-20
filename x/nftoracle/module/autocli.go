763d608353b8a8c5fbbaa6f915ee90705dd0be9f
ddeedev <dee.hamdee.084@gmail.com>
2025-10-20 14:22:28 +0700

    chore(cli): remove autocli coz redundance endpoint on cli

--- x/nftoracle/module/autocli.go
+++ x/nftoracle/module/autocli.go
@@ -7,7 +7,8 @@ import (
 )
 
 // AutoCLIOptions implements the autocli.HasAutoCLIConfig interface.
-func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
+// func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
+func (am AppModule) AutoCLIOption_() *autocliv1.ModuleOptions {
 	return &autocliv1.ModuleOptions{
 		Query: &autocliv1.ServiceCommandDescriptor{
 			Service: modulev1.Query_ServiceDesc.ServiceName,
