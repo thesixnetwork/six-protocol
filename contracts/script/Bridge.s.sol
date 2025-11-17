d44ca0afed1c315a971e3b2c119c05beb04a8e81
ddeedev <dee.hamdee.084@gmail.com>
2025-11-17 18:10:29 +0700

    bug(precompile): tokenfactor merging w/o function transferToCrossChain

--- contracts/script/Bridge.s.sol
+++ contracts/script/Bridge.s.sol
@@ -35,3 +35,64 @@ contract SendToCosmosScript is Script {
     vm.stopBroadcast();
   }
 }
+
+contract UnwrapStakeScript is Script {
+  address ownerAddress;
+  uint64 currentNonce;
+
+  function setUp() public {
+    ownerAddress = vm.envAddress("OWNER");
+    currentNonce = vm.getNonce(ownerAddress);
+  }
+
+  function run() external {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
+
+    // Initialize variables
+    address contractAddress = TOKENFACTORY_PRECOMPILE_ADDRESS;
+    uint256 amount = 1999 * 1e6;
+
+    // Execute the transaction
+    (bool success, ) = contractAddress.call(abi.encodeWithSignature("unwrapStakeToken(uint256)", amount));
+
+    require(success, "Transaction failed");
+
+    // Log the success message
+    console.log("Transfer success!");
+
+    vm.stopBroadcast();
+  }
+}
+
+contract SendToCrossChainScript is Script {
+  address ownerAddress;
+  uint64 currentNonce;
+
+  function setUp() public {
+    ownerAddress = vm.envAddress("OWNER");
+    currentNonce = vm.getNonce(ownerAddress);
+  }
+
+  function run() external {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
+
+    // Initialize variables
+    address contractAddress = TOKENFACTORY_PRECOMPILE_ADDRESS;
+    string memory bridgeInAddress = "6x1kch0sdjr5tuvjh0h3a55c6l5sr6m0phjeag9f2";
+    uint256 amount = 1999 * 1e18;
+    string memory memo = "six-eth-bridge-001";
+    string memory chain = "eth";
+
+    // Execute the transaction
+    (bool success, ) = contractAddress.call(abi.encodeWithSignature("transferToCrossChain(string,uint256,string,string)", bridgeInAddress,amount, memo, chain));
+
+    require(success, "Transaction failed");
+
+    // Log the success message
+    console.log("Transfer success!");
+
+    vm.stopBroadcast();
+  }
+}
